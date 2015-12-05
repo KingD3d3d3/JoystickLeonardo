0977b04ea106d41c26a4b135ec6e03544eb29621
LuomuJie <j.bailet@hotmail.fr>
2015-12-05 17:53:32 +0100

    Handle autofire mode

--- JoystickLeonardo.ino
+++ JoystickLeonardo.ino
@@ -7,12 +7,12 @@ const int buttonsCount = 6;
 
 // Arduino PINs to use
 const int pins[buttonsCount] = {
-    4,
-    7,
+    4, // L
+    7, // R
     8,
     9,
     10, //joystick button : SEL 
-    12 
+    12, // Start
 };
 
 // Connections to joystick (analog) :
@@ -25,7 +25,27 @@ const long debounceDelay = 50;
 bool status[buttonsCount] = {HIGH};
 long lastDebounces[buttonsCount] = {0};
 
+// Number of autofire to handle
+const int buttonsCountAuto = 2;
+
+// Autofire buttons
+const int autoButtons[buttonsCountAuto] = {
+    2,
+    3,
+};
+
+//Potentiometers
+const int potarPins[buttonsCountAuto]{
+  A2,
+  A3,
+};
+int potarValue[buttonsCountAuto] = {}; //store the value coming from the potentiometer
+
+// Store last time
+unsigned long previousMillis[buttonsCountAuto] = {0};
+
 void setup() {
+      Serial.begin(9600);
       
       if (testAutoSendMode){
         Joystick.begin();
@@ -38,6 +58,7 @@ void setup() {
       for (int i = 0; i < buttonsCount; ++i) {
          pinMode(pins[i], INPUT_PULLUP);
       }
+      
 }
 
 
@@ -49,14 +70,37 @@ void loop() {
      Joystick.setXAxis(x); 
      Joystick.setYAxis(y);
      
-      for (int i = 0; i < buttonsCount; ++i) {
-	  handleButton(i, digitalRead(pins[i]), millis());
-      }
-      
-      if (!testAutoSendMode)
-      {
-        Joystick.sendState();
-      }
+     for (int i = 0; i < buttonsCount; ++i) { //i set button number
+	       handleButton(i, digitalRead(pins[i]), millis());
+     }
+
+     for (int i = 0; i < buttonsCountAuto; ++i) {
+        /*
+        potarValue[i] = analogRead(potarPins[i]);
+        Serial.print("potarValue : ");
+        Serial.print(i);
+        Serial.print(" = ");
+        Serial.println(potarValue[i]);
+        */
+        potarValue[i] = analogRead(potarPins[i]);
+        if(potarValue[i] <= 1000){
+          unsigned long period = definePeriod(potarValue[i]);
+          handleAutoButton(autoButtons[i], i, millis(), period);
+        }
+     }
+     
+     /* Check function 
+     potarValue = analogRead(potarPin);
+     //unsigned long period = definePeriod(potarValue);
+     Serial.print("potarValue : ");
+     Serial.println(potarValue);
+     */
+     
+     if (!testAutoSendMode)
+     {
+      Joystick.sendState();
+     }
+     
 }
 
 /**
@@ -66,15 +110,45 @@ void loop() {
  * \param pinStatus PIN status (LOW/HIGH)
  * \param now Time in millis
  */
-
 void handleButton(const int buttonNumber, const int pinStatus, const long now) {
 	if (pinStatus != status[buttonNumber] && now - debounceDelay > lastDebounces[buttonNumber]) {
 		status[buttonNumber] = pinStatus;
 		if (pinStatus == LOW) {
+      /*
+      Serial.print("button : ");
+      Serial.print(buttonNumber);
+      Serial.println(" is pressed");
+      */
 			Joystick.pressButton(buttonNumber);
 		} else {
 			Joystick.releaseButton(buttonNumber);
 		}
 		lastDebounces[buttonNumber] = now;
 	}
+}
+
+/**
+ * \brief Autofire a button with a specific period
+ *
+ * \param buttonNumber Button ID to handle
+ * \param previous Previous time in millis
+ * \param now Current time in millis
+ * \param period Autofire period in millis
+ */
+void handleAutoButton(const int buttonNumber, const int previous, const long now, const long period) {
+   if(now - previousMillis[previous] >= period) {
+     previousMillis[previous] = now;
+     Joystick.pressButton(buttonNumber);
+   } else if(now - previousMillis[previous] >= debounceDelay){ //simulate press delay of a button
+     Joystick.releaseButton(buttonNumber);
+   }
+}
+
+/**
+ * \brief Define period in millisecond from an analog value in input
+ *
+ * \param val Analog value
+ */ 
+unsigned long definePeriod(int val){
+  return 2 * val;
 }
