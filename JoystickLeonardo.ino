f47e8c612ad17c0c2cd38fa459e08ce002697500
LuomuJie <j.bailet@hotmail.fr>
2016-01-19 17:30:53 +0100

    Added USB Mouse control

--- JoystickLeonardo.ino
+++ JoystickLeonardo.ino
@@ -39,18 +39,15 @@ const int potarPins[buttonsCountAuto]{
   A1,
 };
 int potarValue[buttonsCountAuto] = {}; //store the value coming from the potentiometer
-
 // Store last time
 unsigned long previousMillis[buttonsCountAuto] = {0};
 
-// Number of buttons to handle
-const int joyCount = 4;
-// Debounce delay Joystick
-const long debounceDelayJoy = 125;
-long lastDebouncesJoy[joyCount] = {0};
+int incomingInt = 0; // store incoming serial data from Mouse/JoystickUSB/TrackBall on Raspberry Pi
+int x1 = 0, y1 = 0;
 
 void setup() {
       Serial.begin(9600);
+      Serial1.begin(9600);
       
       if (testAutoSendMode){
         Joystick.begin();
@@ -68,16 +65,54 @@ void setup() {
       for (int j = 2; j < 6; ++j) {
          pinMode(j, INPUT_PULLUP);
       }
+    
 }
 
 
 void loop() {
-     
-     int x = defineJoystickX(4,5);
-     int y = defineJoystickY(2,3);
-     Joystick.setXAxis(x);
-     Joystick.setYAxis(y);
 
+     int x0 = defineJoystickX(4,5);
+     int y0 = defineJoystickY(2,3);
+
+     //int buf1 = x1;
+     //int buf2 = y1;
+     // send data only when you receive data:
+     if (Serial1.available() == 2) {
+          // read incoming 2 bytes
+          incomingInt = Serial1.read()*256 + Serial1.read();
+          x1 = highByte(incomingInt);
+          y1 = lowByte(incomingInt);
+          x1 = x1 - 127;
+          y1 = 127 - y1;
+          if(abs(x1)<127 && abs(y1) < 127){
+            x1 = 0;
+            y1 = 0;
+          }
+          /*Serial.print("x1 = ");
+          Serial.print(x1, DEC);
+          Serial.print(", y1 = ");
+          Serial.println(y1, DEC);*/
+      } else if (Serial1.available() > 2){
+          Serial.println("Problem too much data ...");
+          flushSerial1();
+          Serial.println("No More Problem !");
+      } /*else if(Serial1.available() ==0){
+        x1 = 0;
+        y1 = 0;
+      }*/
+      /*if ((buf1==x1) && (buf2==y1)){
+        x1=0;
+        y1=0;
+      }*/
+
+     if((x0 != 0) || (y0 != 0)){
+          Joystick.setXAxis(x0);
+          Joystick.setYAxis(y0);
+     } else if((x0 == 0) && (y0 == 0)){
+          Joystick.setXAxis(x1);
+          Joystick.setYAxis(y1);
+     }
+    
      //Buttons loop
      for (int i = 0; i < buttonsCount; ++i) { //i set button number
 	       handleButton(i, digitalRead(pins[i]), millis());
@@ -98,12 +133,11 @@ void loop() {
      Serial.print("potarValue : ");
      Serial.println(potarValue);
      */
-  
+
      if (!testAutoSendMode)
      {
       Joystick.sendState();
      }
-     
 }
 
 /**
@@ -134,7 +168,7 @@ void handleButton(const int buttonNumber, const int pinStatus, const long now) {
  * \brief Autofire a button with a specific period
  *
  * \param buttonNumber Button ID to handle
- * \param previous Previous time in millis
+ * \param previous Indicator of the previous time in the array
  * \param now Current time in millis
  * \param period Autofire period in millis
  */
@@ -182,5 +216,14 @@ int defineJoystickY(int pinUp, int pinDown){
     //Serial.println("down y= 127");
     return 127;
   } else return 0;
+}
+
+/**
+ * \brief Flush the serial 1
+ */ 
+void flushSerial1(){
+  while(Serial1.available()) {
+      Serial1.read();
+  }
 }
 
